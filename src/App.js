8565150b9ad73b80780f54136cd1a4e7b91ce41d
zinukk <slsxpseh54@naver.com>
2022-04-25 13:53:13 +0900

    feat.settingsComplete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import logo from "./logo.svg";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 import "./App.css";
 import React from "react";
+import { Route } from "react-router-dom";
+import { ConnectedRouter } from "connected-react-router";
+import { history } from "./redux/configStore";
 
 function App() {
-  return <>안녕하세요</>;
+  return (
+    <React.Fragment>
+      <ConnectedRouter history={history}>
+        <Route path="/login" exact component={Login} />
+      </ConnectedRouter>
+    </React.Fragment>
+  );
 }
 
 export default App;
