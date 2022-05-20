c2b4b38ca63c4fb576c74463bb473874c049d299
LeeSangMin12 <52191287+LeeSangMin12@users.noreply.github.com>
2022-05-21 07:43:39 +0900

    feat.notDisappearData/ConfigStore
    리덕스에 있는 데이터를 불러오지 못해 새로고침 할때마다 데이터가 날라가는 오류가 있었는데 persistStore를 이용해서 로컬에 데이터를 저장해 해결하였다!!!

--- src/index.js
+++ src/index.js
@@ -4,10 +4,18 @@ import "./index.css";
 import App from "./App";
 import store from "./redux/configStore";
 import { Provider } from "react-redux";
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
+
+let persistor = persistStore(store)
+
+// import save from './src/redux/configStore'
 
 ReactDOM.render(
   <Provider store={store}>
+     <PersistGate loading={null} persistor={persistor}>
     <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
