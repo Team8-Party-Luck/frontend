2077c759f515d49f35d1a031b39477505bf3c97e
LeeSangMin12 <52191287+LeeSangMin12@users.noreply.github.com>
2022-05-24 03:29:29 +0900

    Update api.js

--- src/shared/api.js
+++ src/shared/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://epocle.shop", //우창님
+  baseURL: "https://epocle.shop", //우창님
   // baseURL: "http://13.125.216.238", //형빈님
-  baseURL: "http://54.180.88.119", //로컬
+  // baseURL: "http://54.180.88.119", //로컬
 
 });
 
