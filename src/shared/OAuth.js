cf8c062872720adf0ae87fbbd9199e0deebb2feb
LeeSangMin12 <52191287+LeeSangMin12@users.noreply.github.com>
2022-05-23 22:30:45 +0900

    Update OAuth.js

--- src/shared/OAuth.js
+++ src/shared/OAuth.js
@@ -9,14 +9,11 @@ const CLIENT_ID = "69e09d88a64e1a73b8fe2357f551ef81";
 // const REDIRECT_URI = "https://www.eatsring.com/auth/kakao"; 
 // 로컬
 // const REDIRECT_URI = "http://13.125.216.238//auth/kakao"; 
-const REDIRECT_URI = "https://eatsring.com/auth/kakao"; 
+const REDIRECT_URI = "https://www.eatsring.com/auth/kakao"; 
 
 
 
-// export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
-
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=facc99e1cf8371cff3624dcd61f92de2&redirect_uri=https://epocle.shop/auth/kakao&response_type=code`;
-
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
 
 // https://kauth.kakao.com/oauth/authorize?client_id=c4f01d9a0b79af30e7fa9225a1419b3b&redirect_uri=http://localhost:3000/user/kakao/callback&response_type=code
