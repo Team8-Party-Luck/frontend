84d2b906970a6006e4b6f2daa7dae4af5d307070
LeeSangMin12 <52191287+LeeSangMin12@users.noreply.github.com>
2022-05-24 00:06:14 +0900

    fix

--- src/shared/OAuth.js
+++ src/shared/OAuth.js
@@ -15,7 +15,7 @@ const REDIRECT_URI = "https://www.eatsring.com/auth/kakao";
 
 
 
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=69e09d88a64e1a73b8fe2357f551ef81&redirect_uri=https://www.eatsring.com/auth/kakao&response_type=code`;
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
 
 // https://kauth.kakao.com/oauth/authorize?client_id=c4f01d9a0b79af30e7fa9225a1419b3b&redirect_uri=http://localhost:3000/user/kakao/callback&response_type=code
