1e217e0d769f2287e6c5431a6aa4fa3cbdd15ac9
zinukk <slsxpseh54@naver.com>
2022-06-02 23:05:42 +0900

    feat.removeComponents

--- src/pages/Alarm.js
+++ src/pages/Alarm.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import AlarmCard from '../components/Alarm/AlarmCard';
-import HeaderNav from '../shared/HeaderNav';
+import React from "react";
+import AlarmCard from "../components/Alarm/AlarmCard";
+// import HeaderNav from '../shared/HeaderNav';
 
 // import { actionCreators as userActions } from "../../redux/modules/user";
 
 const Alarm = () => {
-  return(<React.Fragment>
-    <HeaderNav name="알림"/>
-    <AlarmCard/>
-    <AlarmCard/>
-  </React.Fragment>)
-}
+  return (
+    <React.Fragment>
+      {/* <HeaderNav name="알림"/> */}
+      <AlarmCard />
+      <AlarmCard />
+    </React.Fragment>
+  );
+};
 
 export default Alarm;
